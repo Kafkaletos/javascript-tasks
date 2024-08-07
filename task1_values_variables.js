b425c735efdad0044cd43b8324dac1b9214a68f7
Vassilis Kafkaletos <132747456+Kafkaletos@users.noreply.github.com>
2024-08-07 20:05:33 +0300

    Create task1_values_variables.js

---
+++ task1_values_variables.js
@@ -0,0 +1,7 @@
+let myName = 'Bill';
+let myAge = 40;
+const myCountry = "Greece";
+console.log(`My name: ${myName}`);
+console.log(`My age: ${myAge}`);
+console.log(`My country: ${myCountry}`);
+console.log("\n")
