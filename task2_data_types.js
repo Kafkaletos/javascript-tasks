0914b76feaf50dd12e5bc9ef349f6df04352faf0
Vassilis Kafkaletos <132747456+Kafkaletos@users.noreply.github.com>
2024-08-07 20:07:04 +0300

    Create task2_data_types.js

---
+++ task2_data_types.js
@@ -0,0 +1,11 @@
+let numberVar = 13;
+let stringVar = "myString";
+let booleanVar = true;
+let arrayVar = ["apple", "banana", "cherry"];
+let objectVar = { name: "Alice", age: 28 };
+
+console.log(`numberVAr is of type ${typeof numberVar}`);
+console.log(`stringVar is of type ${typeof stringVar}`);
+console.log(`booleanVar is of type ${typeof booleanVar}`);
+console.log(`arrayVAr is of type ${typeof arrayVar}`);
+console.log(`objectVar is of type ${typeof objectVar}`);
